e74869188cce86e803d959306fdba6adf2d2da06
Maxime ROUX <r.maxime@yandex.com>
2021-10-01 14:42:37 +0200

    View: screen AddIngredientViewController with all elements

---
+++ Reciplease/AddIngredientViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by ROUX Maxime on 01/10/2021.
+//
+
+import UIKit
+
+class AddIngredientViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak var ingredientToAddTextField: UITextField!
+    @IBOutlet weak var ingredientsListTextView: UITextView!
+    @IBOutlet weak var searchForRecipesButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var addIngredientButton: UIButton!
+    
+    
+    // MARK: - Actions
+
+    @IBAction func clearButton(_ sender: UIButton) {
+    }
+    @IBAction func addIngredientButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
